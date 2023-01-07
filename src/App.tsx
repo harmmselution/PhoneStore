81abf7a78098b3998e14889a1d8b4548b768ab6c
harmmselution <bugkfbdhdh@gmail.com>
2023-01-07 18:14:55 +0300

    so commits finally work and can be shown in the contributions

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import './scss/index.scss';
-import { Route, Routes } from 'react-router-dom';
-import Home from './components/Home/Home';
-import Cart from './components/Cart/Cart';
-import { Header } from './components/Header/Header';
-function App() {
-  return (
-    <div className="mainWrapper">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/cart" element={<Cart />}></Route>
-      </Routes>
-    </div>
-  );
-}
+// import './scss/index.scss';
+// import { Route, Routes } from 'react-router-dom';
+// import Home from './components/Home/Home';
+// import Cart from './components/Cart/Cart';
+// import { Header } from './components/Header/Header';
+// function App() {
+//   return (
+//     <div className="mainWrapper">
+//       <Header />
+//       <Routes>
+//         <Route path="/" element={<Home />}></Route>
+//         <Route path="/cart" element={<Cart />}></Route>
+//       </Routes>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
